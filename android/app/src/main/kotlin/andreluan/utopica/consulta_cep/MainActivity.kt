9c78536b56e81d062a79412c9dec25a7492ec17c
André Luan <68342516+aluanbh@users.noreply.github.com>
2021-11-30 16:03:25 -0300

    Initial commit

---
+++ android/app/src/main/kotlin/andreluan/utopica/consulta_cep/MainActivity.kt
@@ -0,0 +1,6 @@
+package andreluan.utopica.consulta_cep
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,9 @@
+import 'package:consulta_cep/Home.dart';
+import 'package:flutter/material.dart';
+
+void main(){
+  runApp(new MaterialApp(
+    debugShowCheckedModeBanner: false,
+    home: Home(),
+  ));
+}
